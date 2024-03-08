cdea7077c07cdaf231929557ad4993fd36ea3183
Lucas Polesello <lucas.c4d@gmail.com>
2024-03-08 18:51:05 -0300

    feat(wait-traefik): fixed script to wait traefik to run deployment

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 run:
+	k3d cluster create --api-port 6550 -p "8081:80@loadbalancer" --agents 1
 	sh wait-traefik.sh
 	kustomize build apps > apps/build.yaml
 	kustomize build . | kubectl apply -f -
 
 cluster:
 	k3d cluster create --api-port 6550 -p "8081:80@loadbalancer" --agents 1
+
+down:
+	k3d cluster delete
--- wait-traefik.sh
+++ wait-traefik.sh
@@ -1,12 +1,20 @@
 #! /bin/bash
+set -e
+
+kubectl proxy --port=8001 &
+
+echo "Waiting kubectl proxy to be running...5 seconds"
+sleep 5
+
 function fetch_stats(){
-  $(curl "http://localhost:8001/api/v1/namespaces/kube-system/pods?fieldSelector=status.phase%3DCompleted&limit=500" | jq '.items[].metadata.name | select(. | contains("svclb-traefik"))')
+  curl -s "http://localhost:8001/api/v1/namespaces/kube-system/pods?fieldSelector=status.phase%3DRunning&limit=500" | jq '.items[].metadata.name | select(. | contains("svclb-traefik"))'
 }
 
-
-result=fetch_stats
+echo "Waiting fetching current state for traefik deployment"
+result=$(fetch_stats)
 
 if [ "${result}" ]; then
+  echo "$result"
   echo "Traefik is running - we can apply changes";
   exit 0
 fi
@@ -15,7 +23,11 @@ while [ -z "${result}" ]
 do
   echo "Waiting 10 seconds to fetch svclb-traefik stats. We can only apply k8s changes after Traefik is running"
   sleep 10
-  result=fetch_stats;
+  result=$(fetch_stats);
 done
+
+
+echo "Finishing kubectl proxy"
+pgrep kubectl proxy | xargs -n 1 kill -9
 
 echo "Traefik is running - we can apply changes"
